0e0b454a8f40a7811d7348000affc1a9c139a819
bioc1301 <joshua.titlow@bioch.ox.ac.uk>
2019-05-29 11:45:25 +0100

    chromagnon bash

--- chromagnon_bash.py
+++ chromagnon_bash.py
@@ -6,24 +6,24 @@
 
 import subprocess
 import os
-import pandas as pd
 
-# specify directory of images
-#ref_dir = ('/usr/people/bioc1301/tmp/chromagnon/cal/')
-#ref_files = os.listdir(ref_dir)
-#targ_dir = ('/usr/people/bioc1301/tmp/chromagnon/aligned/')
-#targ_files = os.listdir(targ_dir)
+# specify directory of reference and target images
+ref_dir = ('/usr/people/bioc1301/tmp/chromagnon/cal/')
+ref_files = os.listdir(ref_dir)
+targ_dir = ('/usr/people/bioc1301/tmp/chromagnon/aligned/')
+targ_files = os.listdir(targ_dir)
 
 ref_file = ('/usr/people/bioc1301/tmp/chromagnon/list.csv')
 file_list = pd.read_csv(ref_file)
 ref_dir = ('/usr/people/bioc1301/data/20190517_AdultBrain_smFISH_MB077c_CamKIIYFP_learning/cal')
 targ_dir = ('/usr/people/bioc1301/data/20190517_AdultBrain_smFISH_MB077c_CamKIIYFP_learning/aligned')
 
-for index, row in file_list.iterrows():
-    print ('converting :',row[0])
-    targ = os.path.join(targ_dir, row[0].replace('_cal.tif','.tif'))
-    ref = os.path.join(ref_dir, row[0])
-
+for file in file_list.iterrows():
+    print ('aligning:',file)
+    targ = os.path.join(targ_dir, file.replace('_cal.tif','.tif'))
+    ref = os.path.join(ref_dir, file)
+    
+    # call chromagnon from bash
     export_command = "chromagnon "+targ+" -R "+ref+" -E dv"
     popen = subprocess.Popen(export_command, shell=True, stdout=subprocess.PIPE)
     out, err = popen.communicate()
